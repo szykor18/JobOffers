9159cddf85d9d284f084c84f1c969466f426b12f
szykor18 <szymonkornik.bs@gmail.com>
2023-11-20 14:58:31 +0100

    added offer rest controller and tests

--- src/integration/java/pl/joboffers/feature/UserWantToSeeOffersIntegrationTest.java
+++ src/integration/java/pl/joboffers/feature/UserWantToSeeOffersIntegrationTest.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -61,11 +62,28 @@ public class UserWantToSeeOffersIntegrationTest extends BaseIntegrationTest impl
         List<OfferDto> offers = objectMapper.readValue(jsonWithOffers, new TypeReference<>() {});
         assertThat(offers).isEmpty();
 
-        //   step 8: there are 2 new offers in external HTTP server
+
+    //   step 8: there are 2 new offers in external HTTP server
     //   step 9: scheduler ran 2nd time and made GET to external server and system added 2 new offers with ids: 1000 and 2000 to database
     //   step 10: user made GET /offers with header “Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 2 offers with ids: 1000 and 2000
+
+
     //   step 11: user made GET /offers/9999 and system returned NOT_FOUND(404) with message “Offer with id 9999 not found”
-    //   step 12: user made GET /offers/1000 and system returned OK(200) with offer
+        //given
+        //when
+        ResultActions performGetOfferWithId9999 = mockMvc.perform(get("/offers/9999"));
+        //then
+        performGetOfferWithId9999.andExpect(status().isNotFound())
+                .andExpect(content().json(
+                """
+                        {
+                        "message": "Offer with id 9999 not found",
+                        "status": "NOT_FOUND"
+                        }
+                """.trim()));
+
+
+        //   step 12: user made GET /offers/1000 and system returned OK(200) with offer
     //   step 13: there are 2 new offers in external HTTP server
     //   step 14: scheduler ran 3rd time and made GET to external server and system added 2 new offers with ids: 3000 and 4000 to database
     //   step 15: user made GET /offers with header “Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 4 offers with ids: 1000,2000, 3000 and 4000
--- src/main/java/pl/joboffers/infrastructure/offer/controller/OfferRestController.java
+++ src/main/java/pl/joboffers/infrastructure/offer/controller/OfferRestController.java
@@ -3,6 +3,7 @@ package pl.joboffers.infrastructure.offer.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.joboffers.domain.offer.OfferFacade;
 import pl.joboffers.domain.offer.dto.OfferDto;
@@ -19,5 +20,11 @@ public class OfferRestController {
     public ResponseEntity<List<OfferDto>> getAllOffers() {
         List<OfferDto> allOffers = offerFacade.findAllOffers();
         return ResponseEntity.ok(allOffers);
+    }
+
+    @GetMapping("/offers/{id}")
+    public ResponseEntity<OfferDto> getOfferById(@PathVariable String id) {
+        OfferDto offerById = offerFacade.findOfferById(id);
+        return ResponseEntity.ok(offerById);
     }
 }
